914776739ba7660a7af2d62e71c9c0d83f2e0464
ganavi12 <ganavi.k@actcorp.local>
2021-05-31 00:24:57 +0530

    create model

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product,Category
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "price", "category"]
+    
+
+
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Category)
